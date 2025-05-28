bb0d60b809deb5470fef62221e319baa975a2b6d
다현 이 <dlekgus0107@gmail.com>
2025-05-29 01:03:14 +0900

    모듈화 진행 begin2

---
+++ menu.c
@@ -0,0 +1,5 @@
+#include "menu.h"
+
+int Menu() {
+    // 메뉴 UI 구현
+}
---
+++ statistics.c
@@ -0,0 +1,11 @@
+#include "statistics.h"
+
+int showUserStatus(int problemNum, char ID[]) {
+    // 유저별 상태 구현
+}
+void showTotalStatus(int problemNum) {
+    // 전체 상태 구현
+}
+void calculateAverage() {
+    // 평균 계산 구현
+}
