450b2cff6b09745d51a774f47ae12f21bd85f958
다현 이 <dlekgus0107@gmail.com>
2025-06-03 22:24:19 +0900

    주석

--- main.c
+++ main.c
@@ -3,32 +3,36 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#include "utils.h"
-#include "user.h"
-#include "problem.h"
-#include "statistics.h"
-#include "menu.h"
+#include "utils.h"        // 화면 지우기 등 유틸리티 함수
+#include "user.h"         // 사용자 로그인 및 정보 관련
+#include "problem.h"      // 문제 관련 함수
+#include "statistics.h"   // 통계 관련 함수
+#include "menu.h"         // 관리자/학생 메뉴 함수
 
-
-
+// 프로그램 시작점
 int main() {
-    int loginStatus = LOGIN_NONE;
-    UserInfo user;
-    clear_screen();
+    int loginStatus = LOGIN_NONE; // 로그인 상태를 저장 (로그인 전 상태로 초기화)
+    UserInfo user;                // 사용자 정보를 저장할 구조체
+
+    clear_screen(); // 화면을 깨끗이 지움 (유틸리티 함수)
+
+    // 로그인 성공할 때까지 반복
     while (loginStatus == LOGIN_NONE) {
-        loginStatus = login(&user);
+        loginStatus = login(&user); // 로그인 시도 -> 결과는 loginStatus에 저장
     }
 
+    // 로그인 상태에 따라 적절한 메뉴 호출
     if (loginStatus == LOGIN_STUDENT) {
-        studentMenu(&user);
+        studentMenu(&user); // 학생 메뉴
     }
     else if (loginStatus == LOGIN_ADMIN) {
-        adminMenu(&user);
+        adminMenu(&user); // 관리자 메뉴
     }
     else {
+        // 로그인 중 오류가 발생한 경우
         printf("Something went Wrong :(\n");
-        return 1;
+        return 1; // 비정상 종료
     }
 
-    return 0;
+    return 0; // 정상 종료
 }
--- utils.h
+++ utils.h
@@ -29,13 +29,13 @@ void clear_screen();
 // 파일에서 한 줄 읽기 (성공 시 true, 실패 시 false)
 bool read_line(FILE* fp, char* buffer, int buffer_size);
 
-// 파일에 한 줄 쓰기 (개행 포함, 성공 시 true)            //안쓰임
+// 파일에 한 줄 쓰기 (개행 포함, 성공 시 true)            //
 bool write_line(FILE* fp, const char* line);
 
-// 파일에서 특정 줄(0-based line_number) 삭제 (성공 시 true)    //안쓰임
+// 파일에서 특정 줄(0-based line_number) 삭제 (성공 시 true)    //
 bool delete_line(const char* filename, int line_number);
 
-// 원하는 경로에 폴더 생성 (성공 시 true)                        //안쓰임
+// 원하는 경로에 폴더 생성 (성공 시 true)                        //
 bool create_folder(const char* dir_path, const char* folder_name);
 
 // 원하는 경로에 C 파일 생성 (성공 시 true)
@@ -44,7 +44,7 @@ bool create_c_file(const char* dir_path, const char* file_name);
 // 원하는 경로에 텍스트 파일 생성 (성공 시 true)
 bool create_text_file(const char* dir_path, const char* file_name);
 
-// C 코드 컴파일,실행하여 출력값을 result에 저장 (성공 시 true)        //안쓰임
+// C 코드 컴파일,실행하여 출력값을 result에 저장 (성공 시 true)        //
 bool compile_and_run_c(const char* c_file_path, char* result, int result_size);
 
 // 현재 시간을 "YYYYMMDD_HHMMSS" 문자열로 반환 (result에 저장, 성공 시 true)
